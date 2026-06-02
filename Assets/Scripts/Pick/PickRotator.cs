901f8fa1ddb289cc4be7dc9ec1c45213ce638ddd
Max Kinet <max.a.kinet@gmail.com>
2026-06-01 17:37:56 -0700

    change rotation direction + get hinge components

--- Assets/Scripts/Pick/PickRotator.cs
+++ Assets/Scripts/Pick/PickRotator.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 public class PickRotator : MonoBehaviour {
 
     private HingeJoint _hingeJoint;
+    private JointMotor _jointMotor;
 
     private void Start() {
-        Debug.Log("Hello, world!");
+        _hingeJoint = GetComponent<HingeJoint>();
+        _jointMotor = _hingeJoint.motor;
+    }
+
+    private void Update() {
+        Debug.Log(_hingeJoint.angle);
     }
 }
