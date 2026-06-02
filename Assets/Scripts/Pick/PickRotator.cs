023f7054c3b61f9132d95f71b971d22b2cc09943
Max Kinet <max.a.kinet@gmail.com>
2026-06-01 22:08:39 -0700

    clean up rotate script

--- Assets/Scripts/Pick/PickRotator.cs
+++ Assets/Scripts/Pick/PickRotator.cs
@@ -3,45 +3,22 @@ using UnityEngine;
 
 public class PickRotator : MonoBehaviour {
 
-    [SerializeField] int MAX_ANGLE = 0;
-    [SerializeField] int MIN_ANGLE = 0;
-    private HingeJoint _hingeJoint;
-    private JointMotor _jointMotor;
-
     private float _lastFrameMouseX = 0;
 
     private InputActionAsset _actionMap;
 
     private void Start() {
-        _hingeJoint = GetComponent<HingeJoint>();
-        _jointMotor = _hingeJoint.motor;
-
         SubscribeToAction();
-        SetMotorAttributes();
     }
 
     private void RotatePick(InputAction.CallbackContext value) {
         float mouseX = value.ReadValue<float>();
         float offset = mouseX - _lastFrameMouseX;
 
-        var hingeLimits = _hingeJoint.limits;
-        hingeLimits.max = hingeLimits.min = (hingeLimits.max + offset) % 360;
-        _hingeJoint.limits = hingeLimits;
     }
 
 
     private void Update() {
-    }
-
-    private void SetMotorAttributes() {
-        _hingeJoint.useMotor = true;
-        _hingeJoint.useLimits = true;
-        _hingeJoint.extendedLimits = true;
-
-        var hingeLimits = _hingeJoint.limits;
-        hingeLimits.max = MAX_ANGLE;
-        hingeLimits.min = MIN_ANGLE;
-        _hingeJoint.limits = hingeLimits;
     }
 
     private void SubscribeToAction() {
