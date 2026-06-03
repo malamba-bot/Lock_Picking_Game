05e65f62dac2aec8072231b09ee72b61948140c2
Max Kinet <max.a.kinet@gmail.com>
2026-06-03 13:06:42 -0700

    clamp pick rotation

--- Assets/Scripts/Pick/PickRotator.cs
+++ Assets/Scripts/Pick/PickRotator.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public class PickRotator : MonoBehaviour {
 
-    [SerializeField] private float sens = 0.1f;
+    [SerializeField] private float sens = 0.3f;
+    [SerializeField] private float maxAngle = 90;
+    [SerializeField] private float minAngle = -90;
 
     private float _lastFrameMouseX;
 
@@ -16,14 +18,16 @@ public class PickRotator : MonoBehaviour {
         float mouseX = value.ReadValue<float>();
         float offset = (mouseX - _lastFrameMouseX) * sens * -1;
 
-        var currentAngle = transform.eulerAngles;
-        Debug.Log(offset);
+        var currentAngle = transform.eulerAngles.z;
+        if (currentAngle > 180) currentAngle -= 360;
+        Debug.Log(currentAngle);
+        float clampedAngle = Mathf.Clamp(currentAngle + offset, minAngle, maxAngle);
+        offset = clampedAngle - currentAngle;
+
         Vector3 angle = new Vector3(
                 0,0,
                 offset);
         transform.Rotate(angle);
-        Debug.Log($"changed to {angle}");
-
 
         _lastFrameMouseX = mouseX;
 
